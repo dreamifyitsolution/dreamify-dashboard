9f141820c6a655650e08984c2cbebd02c47642ec
er-roshan <roshan.adhikari.se@gmail.com>
2022-08-12 00:23:38 +0545

    Sample Data for product and sidebar items;

---
+++ data/productData.js
@@ -0,0 +1,26 @@
+export const products = [
+	{
+		name: 'Product 1',
+		sku: '01631003',
+		qty: '4',
+		price: '$212.00',
+		rating: '4',
+		status: 'published',
+	},
+	{
+		name: 'Product 2',
+		sku: '01631003',
+		qty: '10',
+		price: '$28.00',
+		rating: '4',
+		status: 'scheduled',
+	},
+	{
+		name: 'Product 3',
+		sku: '01631003',
+		qty: '14',
+		price: '$212.00',
+		rating: '4',
+		status: 'Inactive',
+	},
+];
---
+++ data/sidebarData.js
@@ -0,0 +1,79 @@
+export const sidebar = [
+	{
+		title: 'Dashboard',
+		icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path></svg>',
+		items: [],
+	},
+	{
+		title: 'Catalog',
+		icon: '',
+		items: [
+			{
+				name: 'Products',
+				path: '/products',
+			},
+			{
+				name: 'Categories',
+				path: '/',
+			},
+			{
+				name: 'Add Product',
+				path: '/',
+			},
+			{
+				name: 'Add Categories',
+				path: '/',
+			},
+		],
+	},
+	{
+		title: 'Customers',
+		icon: '',
+		items: [
+			{
+				name: 'Customer Listing',
+				path: '/',
+			},
+			{
+				name: 'Add Customer',
+				path: '/',
+			},
+		],
+	},
+	{
+		title: 'Account',
+		icon: '',
+		items: [
+			{
+				name: 'Overview',
+				path: '/',
+			},
+			{
+				name: 'Settings',
+				path: '/',
+			},
+			{
+				name: 'Billing',
+				path: '/',
+			},
+		],
+	},
+	{
+		title: 'Documents',
+		icon: '',
+		items: [
+			{
+				name: 'Overview',
+				path: '/',
+			},
+			{
+				name: 'Settings',
+				path: '/',
+			},
+			{
+				name: 'Billing',
+				path: '/',
+			},
+		],
+	},
+];
